157f847f347a748c29cc9c2e68a2f594bd51068a
Jack Weilage <jack@weilage.dev>
2025-03-16 15:40:44 -0700

    Fix production requiring a dev env var

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,7 +5,20 @@ import { env } from "$env/dynamic/private";
 import { dev } from "$app/environment";
 import { authenticate } from "./auth_socket";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (dev && !env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+if (
+	!dev &&
+	(!env.DATABASE_DB ||
+		!env.DATABASE_USER ||
+		!env.DATABASE_PASSWORD ||
+		!env.DATABASE_HOST ||
+		!env.CF_CLIENT_ID ||
+		!env.CF_CLIENT_SECRET)
+) {
+	throw new Error(
+		"DATABASE_DB, DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, CF_CLIENT_ID, or CF_CLIENT_SECRET is not set",
+	);
+}
 
 const client = dev
 	? postgres(env.DATABASE_URL)
