44042e0f37ba1885405891f8ad239ea72263a436
Jack Weilage <jack@weilage.dev>
2025-03-24 12:43:40 -0700

    Formatting

--- src/lib/server/drizzle/relations.ts
+++ src/lib/server/drizzle/relations.ts
@@ -1,29 +1,29 @@
 import { relations } from "drizzle-orm/relations";
 import { broadcasts, messages, chatters, broadcasters } from "./schema";
 
-export const messagesRelations = relations(messages, ({one}) => ({
+export const messagesRelations = relations(messages, ({ one }) => ({
 	broadcast: one(broadcasts, {
 		fields: [messages.broadcastId],
-		references: [broadcasts.id]
+		references: [broadcasts.id],
 	}),
 	chatter: one(chatters, {
 		fields: [messages.chatterId],
-		references: [chatters.id]
+		references: [chatters.id],
 	}),
 }));
 
-export const broadcastsRelations = relations(broadcasts, ({one, many}) => ({
+export const broadcastsRelations = relations(broadcasts, ({ one, many }) => ({
 	messages: many(messages),
 	broadcaster: one(broadcasters, {
 		fields: [broadcasts.broadcasterId],
-		references: [broadcasters.id]
+		references: [broadcasters.id],
 	}),
 }));
 
-export const chattersRelations = relations(chatters, ({many}) => ({
+export const chattersRelations = relations(chatters, ({ many }) => ({
 	messages: many(messages),
 }));
 
-export const broadcastersRelations = relations(broadcasters, ({many}) => ({
+export const broadcastersRelations = relations(broadcasters, ({ many }) => ({
 	broadcasts: many(broadcasts),
 }));
