5e66d8d64ed4df80d6b7c34f3999553bb5846c6f
Jack Weilage <jack@weilage.dev>
2025-03-24 12:42:56 -0700

    Pass a database connection to each request via hooks

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import * as schema from "$lib/server/drizzle/schema";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			db: PostgresJsDatabase<typeof schema>;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
