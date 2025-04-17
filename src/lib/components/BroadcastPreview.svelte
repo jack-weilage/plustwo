582ec6ae5f620988d52ab6eda67042bee0d5fcfb
Jack Weilage <jack@weilage.dev>
2025-04-16 21:09:16 -0700

    feat: Create `BroadcastPreview` component

---
+++ src/lib/components/BroadcastPreview.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	let {
+		broadcaster,
+		broadcast,
+	}: {
+		broadcaster: {
+			id: number;
+		};
+		broadcast: {
+			id: number;
+			title: string;
+			startedAt: Date;
+			endedAt: Date | null;
+			score: number;
+		};
+	} = $props();
+
+	function humanTimestamp(duration: number): string {
+		duration /= 1000;
+
+		const units = [];
+		for (const seconds of [60 * 60, 60, 1]) {
+			const count = Math.floor(duration / seconds);
+			duration -= count * seconds;
+
+			// Add a section if there is some amount of that unit or a unit has already been added before it.
+			if (count > 0 || units.length > 0) {
+				if (count < 10 && units.length > 0) {
+					units.push(`0${count}`);
+				} else {
+					units.push(count.toString());
+				}
+			}
+		}
+
+		return units.join(":");
+	}
+</script>
+
+<a
+	href="/broadcasters/{broadcaster.id}/broadcasts/{broadcast.id}"
+	class="hover:bg-foreground hover:text-background flex flex-col gap-2 rounded-xl px-4 py-2 text-sm shadow transition-colors {broadcast.endedAt ||
+		'border-destructive border-2'}"
+>
+	<span class="truncate font-bold">{broadcast.title}</span>
+	<div class="flex flex-nowrap items-center justify-between gap-2">
+		<span>{broadcast.score >= 0 ? "+" : ""}{broadcast.score.toLocaleString()}</span>
+		{#if broadcast.endedAt}
+			<span class="text-muted-foreground">
+				{humanTimestamp(+broadcast.endedAt - +broadcast.startedAt)}
+			</span>
+		{:else}
+			<span class="text-destructive">Live</span>
+		{/if}
+	</div>
+</a>
