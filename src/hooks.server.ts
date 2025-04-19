fd184b30d9dcd8fd3b6b13ec57a1d2d899fccdc1
Jack Weilage <jack@weilage.dev>
2025-04-18 20:43:45 -0700

    perf(server): Re-use connections in dev mode

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,12 @@
-import { connect } from "$lib/server/db";
 import type { Handle } from "@sveltejs/kit";
 
+import { dev } from "$app/environment";
+import { connect } from "$lib/server/db";
+
+const dev_connect = dev ? await connect() : null!;
+
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.db = await connect();
+	event.locals.db = dev ? dev_connect : await connect();
 
 	return await resolve(event);
 };
